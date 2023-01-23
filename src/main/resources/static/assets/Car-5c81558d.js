c9dec00c8f1dcff8c7ed9719849e51acff1a0dc5
Sergey Pavlov <s.a.pavlov@mail.ru>
2023-01-23 20:51:10 +0300

    delete static

--- src/main/resources/static/assets/Car-5c81558d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index-f21f8303.js";const n={};function o(t,a){return c(),r("h3",null,"Данный раздел находится в разработке...")}const _=e(n,[["render",o]]);export{_ as default};
