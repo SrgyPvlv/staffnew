f228dfcd04c479ce31ebedb5ff24a99f83c54259
Sergey Pavlov <s.a.pavlov@mail.ru>
2023-02-26 22:50:35 +0300

    add crossorigin

--- src/main/java/com/example/staff/controller/FunctionGroupEntityController.java
+++ src/main/java/com/example/staff/controller/FunctionGroupEntityController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.example.staff.entity.FunctionGroupEntity;
 import com.example.staff.service.FunctionGroupEntityService;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins="http://localhost:5173/")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
